c0a620190a4ef7b840b16d91e52ba7c71135151a
Elias Leers <eli@hackerguy.net>
2016-09-09 23:29:19 -0700

    Added cleantext function to remove all nonletter characters. Implemented cleantext into existing cipher functions. \nAdded groupfreq function to find frequency of character groups for use in Vigenere cracking. \nWork in progress: groupDistance function for Vigenere cracking. \nTo do: fix function names, they're awful.

--- ciphers.py
+++ ciphers.py
@@ -1,18 +1,20 @@
 #Cipher Tools
 import string
+import re
+
 low = string.ascii_lowercase
 up = string.ascii_uppercase
-def shift(ch, sh):
-	if ch in low:
-		return low[(low.index(ch) + sh) % 26]
-	elif ch in up:
-		return up[(up.index(ch) + sh) % 26]
+def shift(char, shiftVal):
+	if char in low:
+		return low[(low.index(char) + shiftVal) % 26]
+	elif char in up:
+		return up[(up.index(char) + shiftVal) % 26]
 	else:
-		return ch
+		return char
 
-def caesarE(plaintext, sh): return "".join(shift(ch,sh) for ch in plaintext)
+def caesarE(plaintext, sh): return "".join(shift(char,sh) for char in cleantext(plaintext))
 
-def caesarD(plaintext, sh): return encrypt(plaintext, 26 - (sh % 26))
+def caesarD(plaintext, sh): return caesarE(plaintext, 26 - (sh % 26))
 
 def rot13(plaintext): return(caesarE(plaintext, 13))
 
@@ -27,19 +29,19 @@ def substitute(ch, key):
 
 def atbash(plaintext):
 	key = low[::-1]
-	return "".join(substitute(ch, key) for ch in plaintext)
+	return "".join(substitute(char, key) for char in cleantext(plaintext))
 
 def vigenereE(plaintext, key):
-	ct = ''
+	ciphertext = ''
 	ptindex = 0
 	keyindex = 0
 	while ptindex < len(plaintext):
-		ct += shift(plaintext[ptindex], letterindex(key[keyindex]))
+		ciphertext += shift(plaintext[ptindex], letterindex(key[keyindex]))
 		keyindex += 1
 		ptindex += 1
 		if keyindex > len(key):
 			keyindex = 0
-	return ct
+	return ciphertext
 
 def vigenereD(plaintext, key):
 	ct = ''
@@ -59,10 +61,11 @@ def letterindex(ch):
 	else:
 		return -1
 
-def freq(text):
+def letterfreq(text):
 	freq = {}
 
 	for char in text:
+			print(char)
 			if char in freq:
 				freq[char] += 1
 			else:
@@ -72,15 +75,67 @@ def freq(text):
 	return freq
 
 
+def groupfreq(textin, minSize, maxSize):
+	freqarray = []
+	minS = minSize
+	maxS = maxSize+1
+	text = cleantext(textin)
+	for i in range(minS,maxS):
+		currentIndex = 0
+		freq = {}
+		while(currentIndex < len(text)-1):
+			sliced = text[currentIndex:currentIndex+i]
+		
+			if (sliced in freq):
+				freq[sliced] += 1
+			else:
+				freq[sliced] = 1
+			currentIndex += 1
+		freqarray.append(freq)
+
+	return freqarray
+
+def cleantext(textin): #removes all nonletters and capitalizes
+	new = ""
+	text = textin.upper()
+	for char in text:
+		if (char in up):
+			new += char
+	return new;
+
+def groupDistance(textIn):
+	frequency = groupfreq(textIn, 3, 5)
+	for dictpair in frequency:
+		for group in dictpair:
+			if dictpair[group] > 1:
+				print(group, dictpair[group], '\n')
+				kek = [m.start() for m in re.finditer(group, textIn)]
+				print(kek)
+
+
+#Testing
 print("Testing Caesar")
 print(caesarE("Beware the ides of March", 1))
+print(caesarD(caesarE("Beware the ides of March", 1), 1))
 print("Testing Rot13")
 print(rot13("So secure OMG"))
 print("Testing Atbash")
 print(atbash("Super Secure!"))
 
-print(letterindex('v'))
+print(letterindex('z'))
 print(vigenereE("testing", "yourmom"))
 print(vigenereD("RSMKUBS", "yourmom"))
 
-print(vigenereE):
+print(vigenereE("whoops", "yourmom"))
+
+testdata1 = "RIKVBIYBITHUSEVAZMMLTKASRNHPNPZICSWDSVMBIYFQEZUBZPBRGYNTBURMBECZQKBMBPAWIXSOFNUZECNRAZFPHIYBQEOCTTIOXKUNOHMRGCNDDXZWIRDVDRZYAYYICPUYDHCKXQIECIEWUICJNNACSAZZZGACZHMRGXFTILFNNTSDAFGYWLNICFISEAMRMORPGMJLUSTAAKBFLTIBYXGAVDVXPCTSVVRLJENOWWFINZOWEHOSRMQDGYSDOPVXXGPJNRVILZNAREDUYBTVLIDLMSXKYEYVAKAYBPVTDHMTMGITDZRTIOVWQIECEYBNEDPZWKUNDOZRBAHEGQBXURFGMUECNPAIIYURLRIPTFOYBISEOEDZINAISPBTZMNECRIJUFUCMMUUSANMMVICNRHQJMNHPNCEPUSQDMIVYTSZTRGXSPZUVWNORGQJMYNLILUKCPHDBYLNELPHVKYAYYBYXLERMMPBMHHCQKBMHDKMTDMSSJEVWOPNGCJMYRPYQELCDPOPVPBIEZALKZWTOPRYFARATPBHGLWWMXNHPHXVKBAANAVMNLPHMEMMSZHMTXHTFMQVLILOVVULNIWGVFUCGRZZKAUNADVYXUDDJVKAYUYOWLVBEOZFGTHHSPJNKAYICWITDARZPVU"
+# testingfreq = groupfreq(testdata1,3,5)
+
+
+
+# for mm in testingfreq:
+# 	for cc in mm:
+# 		if mm[cc] > 1:
+# 			print(cc, mm[cc], '\n')
+
+groupDistance(testdata1)
